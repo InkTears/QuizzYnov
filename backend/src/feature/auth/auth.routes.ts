880a37a6ae7e0ed6f6a4da1ed0f60a5e71ba9596
Your Name <you@example.com>
2026-04-02 23:06:44 +0200

    feat: Mise en place systeme auth

--- backend/src/feature/auth/auth.routes.ts
+++ backend/src/feature/auth/auth.routes.ts
@@ -1,10 +1,23 @@
 import { Router, Request, Response } from "express";
 import authController from "./auth.controller";
+import { authMiddleware } from "../../middlewares/auth.middleware";
 
 const router = Router();
 
-router.post("/register", (req: Request, res: Response) => authController.register(req, res));
-router.post("/login", (req: Request, res: Response) => authController.login(req, res));
-router.post("/refresh", (req: Request, res: Response) => authController.refresh(req, res));
+router.post("/register", (req: Request, res: Response) =>
+  authController.register(req, res)
+);
+
+router.post("/login", (req: Request, res: Response) =>
+  authController.login(req, res)
+);
+
+router.post("/refresh", (req: Request, res: Response) =>
+  authController.refresh(req, res)
+);
+
+router.post("/logout", authMiddleware, (req: Request, res: Response) =>
+  authController.logout(req, res)
+);
 
 export const authRouter = router;
