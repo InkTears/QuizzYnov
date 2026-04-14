fa060fe7d8de59897406790bf0fc1427bcaaf591
elamine-insanakib <elamine.insa-nakib@laplateforme.io>
2026-04-14 15:48:43 +0200

    suppression affichage debug users

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,10 @@
 import QuizPage from './pages/Quiz_page'
 import { Leaderboard } from './pages/Leaderboard';
 import Home from './pages/Home';
-import { useEffect, useState } from 'react';
-
-interface User {
-    id: number;
-    name: string;
-}
+import { useState } from 'react';
 
 function App() {
-    const [users, setUsers] = useState<User[]>([])
     const [page, setPage] = useState<"quiz" | "leaderboard" | "home">("home");
-
-    useEffect(() => {
-        fetch('/api/users')
-            .then(res => res.json())
-            .then(data => setUsers(data))
-            .catch(err => console.error(err))
-    }, [])
 
 return (
   <div style={{ fontFamily: 'sans-serif', minHeight: '100vh' }}>
@@ -26,11 +13,6 @@ return (
     {page === "quiz" && ( <QuizPage key="quiz" onNavigate={setPage} /> )}
     {page === "leaderboard" && <Leaderboard onNavigate={setPage} />}
 
-    <div style={{ padding: '2rem', borderTop: '1px solid #eee', marginTop: '2rem' }}>
-      <pre style={{ background: '#f4f4f4', padding: '1rem', borderRadius: '8px', fontSize: '0.8rem' }}>
-        {JSON.stringify(users, null, 2)}
-      </pre>
-    </div>
   </div>
 )
 }
