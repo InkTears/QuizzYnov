e0a050368f02df020db804e592807529dd48a41a
Egor <egor.khaybulov@epitech.eu>
2026-04-14 09:20:38 +0200

    Add 404 Not Found page and update routing in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,48 +1,29 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import QuizPage from './pages/Quiz_page'
-
-// Import de tes pages
 import LoginPageUser from './pages/Login_page.tsx';
 import RegisterPageUser from './pages/Register_page_user';
 import TableauDeBoardAdmin from './pages/Tableau_de_board_admin';
 import CRUDQuestionAdmin from './pages/CRUD_question_admin.tsx'
-/*import QuizPage from './pages/Quiz_page';
-
-import Leaderboard from './pages/Leaderboard';*/
-//import ProtectedRoute from './components/ProtectedRoute'; // Ajuste le chemin selon ton choix
-//import Navbar from './modules/Navbar';
+import Leaderboard from './pages/Leaderboard';
+import NotFoundPage from './pages/404';
 import './App.css';
-
-interface User {
-    id: number;
-    name: string;
-}
 
 function App() {
     return (
         <Router>
             <div className="App">
-                {/*<Navbar />*/}
                 <Routes>
-                    {/* Route par défaut : redirige vers le login utilisateur */}
                     <Route path="/" element={<Navigate to="/login" />} />
 
-                    {/* Routes publiques */}
                     <Route path="/login" element={<LoginPageUser />} />
                     <Route path="/register" element={<RegisterPageUser />} />
                     <Route path="/admin/login" element={<Navigate to="/login" replace />} />
 
-                    {/* Routes de l'application (à protéger plus tard) */}
-                    {/*<Route element={<ProtectedRoute />}>*/}
-
                     <Route path="/admin/dashboard" element={<TableauDeBoardAdmin />} />
                     <Route path="/admin/questions" element={<CRUDQuestionAdmin />} />
-                    {/* <Route path="/quiz" element={<QuizPage />} />
+                    <Route path="/quiz" element={<QuizPage />} />
                     <Route path="/leaderboard" element={<Leaderboard />} />
-                </Route> */}
-
-                    {/* Page 404 ou redirection si la route n'existe pas */}
-                    <Route path="*" element={<Navigate to="/login" />} />
+                    <Route path="*" element={<NotFoundPage />} />
                 </Routes>
             </div>
         </Router>
